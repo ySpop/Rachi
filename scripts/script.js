a7be5da6f8dd31da52dd43c48e0a0393230ecf2b
Hugo D. Pina <hugopina99@gmail.com>
2024-03-24 23:38:15 -0300

    Mobile estilização 1.1

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
-  const nav = document.querySelector(".mobileNavBar");
-  const btnNav = document.getElementById("btnMobileNav");
+const nav = document.querySelector(".mobileNavBar");
+const btnNav = document.getElementById("btnMobileNav");
 
-  document.addEventListener("DOMContentLoaded", function () {
-    // Adiciona a classe para ocultar a segunda nav móvel
-    document.querySelector(".mobileNavBar").classList.add("hidden");
-    // Adiciona um evento de clique ao botão que mostra a nav
-    document.getElementById("btnMobileNav").addEventListener("click", function () {
-      // Remove a classe para mostrar a segunda nav móvel
-      document.querySelector(".mobileNavBar").classList.remove("hidden");
-  
-      btnNav.addEventListener("click", () => {
-          nav.classList.toggle("open");
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  // Adiciona a classe para ocultar a segunda nav móvel
+  document.querySelector(".mobileNavBar").classList.add("hidden");
+  // Adiciona um evento de clique ao botão que mostra a nav
+  document.getElementById("btnMobileNav").addEventListener("click", function () {
+    // Remove a classe para mostrar a segunda nav móvel
+    document.querySelector(".mobileNavBar").classList.remove("hidden");
+
+    btnNav.addEventListener("click", () => {
+        nav.classList.toggle("open");
+      });
   });
+});
 
